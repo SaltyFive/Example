e8d5b9ecb43f41c8b34b35c9109e4e900c1b8afe
SaltyFive <xxccyy_aa@163.com>
2026-03-09 21:53:07 +0800

    trpc

--- server/trpc/routers/auth.ts
+++ server/trpc/routers/auth.ts
@@ -1,4 +1,14 @@
-import { router } from "../trpc";
+import z from "zod";
+import { publicProcedure, router } from "../trpc";
+
+type User = {
+  username:string,
+  password:string,
+}
+
+async function printUser(user:User) {
+  console.log(`你好啊，${user.username}`)
+}
 
 export const authRouter = router({
 
